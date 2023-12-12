106fcdaa82ea82d7e13c0253473f3afb6e516374
shashankpatil28 <patilshashank2003@gmail.com>
2023-12-12 21:36:00 +0530

    leaderBoard color

---
+++ src/views/admin/dataTables/components/dummyData.js
@@ -0,0 +1,46 @@
+export const dum = [
+    {
+        id : 1,
+        pos : 1,
+        name : "shashank",
+        uid : "shashank28",
+        points : 0,
+    },
+    {
+        id : 2,
+        pos : 2,
+        name : "shashank",
+        uid : "shashank28",
+        points : 0,
+    },
+    {
+        id : 3,
+        pos : 3,
+        name : "shashank",
+        uid : "shashank28",
+        points : 0,
+    },
+    {
+        id : 4,
+        pos : 4,
+        name : "shashank",
+        uid : "shashank28",
+        points : 0,
+    },
+    {
+        id : 5,
+        pos : 5,
+        name : "shashank",
+        uid : "shashank28",
+        points : 0,
+    },
+    {
+        id : 6,
+        pos : 6,
+        name : "shashank",
+        uid : "shashank28",
+        points : 0,
+    },
+
+]
+    
